c2aa046bb6e8520f148d9f07b75a654b07b801e6
louminiha <miharisoarazanakoto@gmail.com>
2025-06-26 23:01:15 +0300

    stat

---
+++ app/controllers/StatistiqueController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\RessourceMiniereModel;
+use app\models\AccueilModel;
+
+use Exception;
+use Flight;
+
+class StatistiqueController
+{
+
+    public function __construct() {}
+    public function afficherStatistiques()
+    {
+        $db = Flight::db();
+        $model = new RessourceMiniereModel($db);
+
+        $statRegionType = $model->getStatpar_ressource_par_region();
+        $statParType = $model->get_repartition_type_resource();
+        $statParStatut = $model->get_repartition_par_statut();
+        $model = new AccueilModel($db);
+
+        $regions = $model->getRegions();
+        $types = $model->getTypes();
+        $statuts = $model->getStatuts();
+
+       Flight::render('template.php', [
+            'page'=>'statistiques',
+            'regions' => $regions,
+            'types' => $types,
+            'statuts' => $statuts,
+            'regionType' => $statRegionType,
+            'typeRessource' => $statParType,
+            'statutRessource' => $statParStatut
+        ]);
+    }
+    
+}

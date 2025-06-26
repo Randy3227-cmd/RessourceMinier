88296e36c98a6ceda8cdd7947dfae27745d8c5ba
Randy3227 <randyrobinson3227@gmail.com>
2025-06-26 23:56:52 +0300

    f

--- app/models/AccueilModel.php
+++ app/models/AccueilModel.php
@@ -11,6 +11,7 @@ class AccueilModel {
     public function getRegions() {
         return $this->db->fetchAll("SELECT id_region, nom_region FROM region ORDER BY nom_region");
     }
+    
 
     public function getTypes() {
         return $this->db->fetchAll("SELECT id_type, nom_type FROM type_ressource ORDER BY nom_type");
--- sql/INSERT.sql
+++ sql/INSERT.sql
@@ -87,3 +87,34 @@ INSERT INTO ressource_miniere (
 
 
 
+-- mbola
+INSERT INTO ressource_miniere (
+    nom_site, description, id_type_ressource, id_statut_ressource, geom, lien
+) VALUES
+('Norcross Madagascar', 'Mine de nickel et cobalt, grande exploitation industrielle.', 2, 3,
+    ST_SetSRID(ST_MakePoint(47.1237814, -18.809629), 4326), 'https://www.facebook.com/MadagascarMinerals/?locale2=zh_CN&_rdr');
+
+-- oui
+INSERT INTO ressource_miniere (
+    nom_site, description, id_type_ressource, id_statut_ressource, geom, lien
+) VALUES
+('Andrafiamena Andavakoera Protected Area', 'Or alluvionnaire', 1, 3,
+    ST_SetSRID(ST_MakePoint(49.2323121, -13.0079407), 4326), 'https://www.fapbm.org/en/aire_protegee/andrafiamena-andavakoera-2/');
+
+
+INSERT INTO ressource_miniere (
+    nom_site, description, id_type_ressource, id_statut_ressource, geom, lien
+) VALUES
+('Sahamamy project (Tirupati Graphite PLC)', 'Nickel latéritique', 2, 2,
+    ST_SetSRID(ST_MakePoint(48.1058163, -18.5914427), 4326), 'https://tirupatigraphite.co.uk/');
+
+
+INSERT INTO ressource_miniere (
+    nom_site, description, id_type_ressource, id_statut_ressource, geom, lien
+) VALUES
+('Sahamamy project (Tirupati Graphite PLC)', 'Nickel latéritique', 1, 2,
+    ST_SetSRID(ST_MakePoint(47.5075424, -18.9001902), 4326), 'https://www.facebook.com/varotra.isankarazany.1/?locale=fr_FR');
+
+    
+    
+
